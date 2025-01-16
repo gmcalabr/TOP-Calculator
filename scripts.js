c638dc80e93f67a602302d2b73eb9425062ceed6
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2025-01-16 18:45:17 -0500

    First expression math appears to work
    Entering an expression of two numbers and an operator now appears to
    work correctly.

--- scripts.js
+++ scripts.js
@@ -8,18 +8,25 @@ const power = (a, b) => (a ** b);
 let inputA 
 let inputB
 let operator
+let inputs = {
+    inputA: undefined,
+    inputB: undefined,
+    operator: undefined,
+};
+let answer
+let newNum = false;
 
 const buttons = document.querySelectorAll('button');
 const expressionScreen = document.getElementById("expressionScreen");
-const answerScreen = document.querySelector('answerScreen');
+const inputsScreen = document.querySelector('inputsScreen');
 
-function enter(inputA, inputB, operator) {
-    // launch calculation
-}
+
+
 
 
 // subfunctions________________________________________________
 function clearOperators() {
+    inputs.operator = undefined;
     const operatorButtons = document.getElementsByClassName('operator');
     for (let operatorButton of operatorButtons) {
         operatorButton.classList.toggle('highlighted', false);
@@ -29,42 +36,73 @@ function clearOperators() {
 }
 
 function highlightOperator(buttonPress) {
+    inputs.operator = buttonPress.text;
     const operatorButtons = document.getElementsByClassName('operator');
     for (let operatorButton of operatorButtons) {    
         if (operatorButton.innerHTML == buttonPress.text) {
             operatorButton.classList.toggle('highlighted', true);
             // console.log(`${operatorButton.innerHTML} [[[on]]]`)
             // console.log(operatorButton.classList)
+            // console.log(inputs.operator);
         }
     }
 }
 
+function doMath(inputs) {
+    if (inputs.operator == "/") {
+        answer = divide(inputs.inputA, inputs.inputB)
+    } else if (inputs.operator == "x") {
+        answer = multiply(inputs.inputA, inputs.inputB)
+    } else if (inputs.operator == "-") {
+        answer = subtract(inputs.inputA, inputs.inputB)
+    } else if (inputs.operator == "+") {
+        answer = add(inputs.inputA, inputs.inputB)
+    };
+    console.log(answer);
+    clearExpressionScreen();
+    pushExpressionScreen(answer);
+}
 // subfunctions________________________________________________
+
+
+
+
 
 // button press functions______________________________________
 
-function pushExpressionScreen(buttonPress) {
+function pushExpressionScreen(num) {
     let readout = expressionScreen.innerHTML;
-    readout = readout + buttonPress.text;
+    if (newNum) readout = "";
+    readout = readout + num;
     // console.log(readout);
     expressionScreen.innerHTML = readout;
+    newNum = false;
+    // console.log(newNum);
 }
 
-function expressionButton(buttonPress) {
+function operatorButton(buttonPress) {
     clearOperators();
     highlightOperator(buttonPress);
-
+    let readout = expressionScreen.innerHTML;
+    inputs.inputA = readout;
+    // console.log(`inputA = ${inputs.inputA}`);
+    newNum = true;
+    // console.log(newNum);
 }
 
-function pushAnswerScreen(buttonPress) {
-    
+function pushinputsScreen(buttonPress) {
+
+    newNum = false;
 }
 
 function clearExpressionScreen() {
     expressionScreen.innerHTML = "";
+    clearOperators();
+    newNum = false;
+
 }
 
-function clearAnswerScreen(buttonPress) {
+function clearinputsScreen(buttonPress) {
     
 }
 
@@ -72,27 +110,40 @@ function clearData() {
     
 }
 
+function equals() {
+    newNum = false;
+    let readout = expressionScreen.innerHTML;
+    inputs.inputB = readout;
+    console.table(inputs);
+    doMath(inputs);
+}
+
 // button press functions________________________________________
+
+
+
+
+
+
 
 function determineAction(buttonPress) {
         // console.log('Button clicked: ', buttonPress.text)
         // console.log('Button class: ', buttonPress.type)
 
         if (buttonPress.type == "number") {
-            pushExpressionScreen(buttonPress);
+            pushExpressionScreen(buttonPress.text);
         } else if (buttonPress.type == "special") {
-            pushExpressionScreen(buttonPress);
+
         } else if (buttonPress.type == "operator") {
-            expressionButton(buttonPress);
+            operatorButton(buttonPress);
         } else if (buttonPress.type == "clear") {
             clearExpressionScreen();
         } else if (buttonPress.type == "backspace") {
 
         } else if (buttonPress.type == "enter") {
-
+            equals();
         }      
 }
-
 
 buttons.forEach(button => {
     button.addEventListener("click", function() {
