4081635695c908408b2bd07de2ba15b2379cbc59
gmcalabr <175723646+gmcalabr@users.noreply.github.com>
2025-01-17 17:36:31 -0500

    Made significant progress on calculator functions
    All buttons roughly work. Still significant testing/QC, as well as
    style and design improvements to be made.

--- scripts.js
+++ scripts.js
@@ -1,37 +1,30 @@
 
-const add = (a, b) => (a + b);
+const add = (a, b) => (Number(a) + Number(b));
 const subtract = (a, b) => (a - b);
 const multiply = (a, b) => (a * b);
 const divide = (a, b) => (a / b);
 const power = (a, b) => (a ** b);
 
-let inputA 
-let inputB
-let operator
 let inputs = {
     inputA: undefined,
     inputB: undefined,
     operator: undefined,
 };
-let answer
+
 let newNum = false;
 
 const buttons = document.querySelectorAll('button');
-const expressionScreen = document.getElementById("expressionScreen");
-const inputsScreen = document.querySelector('inputsScreen');
-
-
-
-
+const upperDisplay = document.getElementById("upperDisplay");
+const lowerDisplay = document.getElementById("lowerDisplay");
+const display = document.querySelectorAll('display');
 
 // subfunctions________________________________________________
+
 function clearOperators() {
     inputs.operator = undefined;
     const operatorButtons = document.getElementsByClassName('operator');
     for (let operatorButton of operatorButtons) {
         operatorButton.classList.toggle('highlighted', false);
-        // console.log(operatorButton.classList)
-        // console.log(`${operatorButton.innerHTML} [[[off]]]`)
     }
 }
 
@@ -41,15 +34,17 @@ function highlightOperator(buttonPress) {
     for (let operatorButton of operatorButtons) {    
         if (operatorButton.innerHTML == buttonPress.text) {
             operatorButton.classList.toggle('highlighted', true);
-            // console.log(`${operatorButton.innerHTML} [[[on]]]`)
-            // console.log(operatorButton.classList)
-            // console.log(inputs.operator);
         }
     }
 }
 
 function doMath(inputs) {
-    if (inputs.operator == "/") {
+    let answer;
+    if (inputs.inputB == 0 && inputs.operator == "/") {
+        let message = "You broke it. You know what you did...";
+        return message;
+    }
+        if (inputs.operator == "/") {
         answer = divide(inputs.inputA, inputs.inputB)
     } else if (inputs.operator == "x") {
         answer = multiply(inputs.inputA, inputs.inputB)
@@ -58,91 +53,123 @@ function doMath(inputs) {
     } else if (inputs.operator == "+") {
         answer = add(inputs.inputA, inputs.inputB)
     };
-    console.log(answer);
-    clearExpressionScreen();
-    pushExpressionScreen(answer);
+    return answer;
 }
+
+function pushlowerDisplay(str) {
+    let readout = lowerDisplay.innerHTML;
+    if (newNum) readout = "";
+    readout = readout + str;
+    lowerDisplay.innerHTML = readout;
+    newNum = false;
+}
+
+function clearlowerDisplay() {
+    lowerDisplay.innerHTML = "";
+    clearOperators();
+    newNum = false;
+}
+
+function clearupperDisplay(buttonPress) {
+    upperDisplay.innerHTML = "";
+}
+
+function generateRandom() {
+    let randomNumber = Math.round(Math.random() * 100000) / 100000;
+    return randomNumber
+}
+
 // subfunctions________________________________________________
-
-
-
-
 
 // button press functions______________________________________
 
-function pushExpressionScreen(num) {
-    let readout = expressionScreen.innerHTML;
-    if (newNum) readout = "";
-    readout = readout + num;
-    // console.log(readout);
-    expressionScreen.innerHTML = readout;
-    newNum = false;
-    // console.log(newNum);
-}
-
 function operatorButton(buttonPress) {
-    clearOperators();
-    highlightOperator(buttonPress);
-    let readout = expressionScreen.innerHTML;
-    inputs.inputA = readout;
-    // console.log(`inputA = ${inputs.inputA}`);
-    newNum = true;
-    // console.log(newNum);
+
+    let readout = lowerDisplay.innerHTML;
+    if (readout) {    
+        clearOperators();
+        highlightOperator(buttonPress);
+        inputs.inputA = readout;
+        newNum = true;
+    };
 }
 
-function pushinputsScreen(buttonPress) {
+function specialButton(string) {
 
-    newNum = false;
-}
+    if (string == "BS") {
+        let readout = lowerDisplay.innerHTML;
+        if (readout) {
+            readout = readout.slice(0, -1);
+            lowerDisplay.innerHTML = readout;
+        }
 
-function clearExpressionScreen() {
-    expressionScreen.innerHTML = "";
-    clearOperators();
-    newNum = false;
-
-}
-
-function clearinputsScreen(buttonPress) {
-    
-}
-
-function clearData() {
-    
+    } else if (string == "+/-") {
+        let readout = lowerDisplay.innerHTML;
+        if (!newNum) {
+            if (readout.charAt(0) != "-") {
+                readout = `-${readout}`
+                lowerDisplay.innerHTML = readout;
+            } else {
+                readout = readout.slice(0 + 1);
+                lowerDisplay.innerHTML = readout;
+            }
+        }
+    } else if (string == "Rand") {
+        let readout = generateRandom();
+        lowerDisplay.innerHTML = readout;
+    }
 }
 
 function equals() {
-    newNum = false;
-    let readout = expressionScreen.innerHTML;
-    inputs.inputB = readout;
+
     console.table(inputs);
-    doMath(inputs);
+    //if there is no expression or inputB, then skip everything
+
+    let readout = lowerDisplay.innerHTML;
+    inputs.inputB = readout;
+
+    if (inputs.inputA && inputs.inputB && inputs.operator) {
+        newNum = true;
+    
+        let expressionPrintout = `${inputs.inputA} ${inputs.operator} ${inputs.inputB}`
+        upperDisplay.innerHTML = expressionPrintout;
+        let answer = doMath(inputs);
+    
+        pushlowerDisplay(answer);
+        inputs.inputA = answer;
+        inputs.inputB = undefined;
+        inputs.operator = undefined;
+    
+        clearOperators();
+    }
+
+}
+
+function clearButton() {
+    clearlowerDisplay();
+    clearupperDisplay();
+
+    for (let key in inputs) {
+        inputs[key] = undefined;
+    }
+
+    newNum = false;
 }
 
 // button press functions________________________________________
 
-
-
-
-
-
-
 function determineAction(buttonPress) {
-        // console.log('Button clicked: ', buttonPress.text)
-        // console.log('Button class: ', buttonPress.type)
-
         if (buttonPress.type == "number") {
-            pushExpressionScreen(buttonPress.text);
+            pushlowerDisplay(buttonPress.text);
         } else if (buttonPress.type == "special") {
-
+            specialButton(buttonPress.text);
         } else if (buttonPress.type == "operator") {
             operatorButton(buttonPress);
         } else if (buttonPress.type == "clear") {
-            clearExpressionScreen();
-        } else if (buttonPress.type == "backspace") {
-
-        } else if (buttonPress.type == "enter") {
+            clearButton();
+        } else if (buttonPress.type == "equals") {
             equals();
-        }      
+        }
 }
 
 buttons.forEach(button => {
@@ -150,10 +177,7 @@ buttons.forEach(button => {
         buttonPress = {
             text: this.innerHTML,
             type: this.className,
-        }
-        // console.log('Button clicked: ', buttonPress.text)
-        // console.log('Button class: ', buttonPress.type)
-        
+        }        
         determineAction(buttonPress);
     })
 })
